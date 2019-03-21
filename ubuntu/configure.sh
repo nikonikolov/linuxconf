d855f6fe4886253c22218c7389821726e338af23
nikonikolov <niko.b.nikolov@gmail.com>
2019-03-21 23:30:09 +0000

    use apt instead of apt-get

--- ubuntu/configure.sh
+++ ubuntu/configure.sh
@@ -20,8 +20,8 @@ configure_tmux $UBUNTU_BACKUP_DIR
 # ------------- Install sublime text 3 -------------
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt-get install -y sublime-text
+sudo apt update
+sudo apt install -y sublime-text
 # Configure shortcuts and options
 configure_sublime $UBUNTU_BACKUP_DIR
 
@@ -64,8 +64,8 @@ else
   # Install unity-tweak-tool - needed for number of workspaces
   # If not found, add this repo
   # sudo add-apt-repository ppa:freyja-dev/unity-tweak-tool-daily
-  sudo apt-get update
-  sudo apt-get install -y unity-tweak-tool
+  sudo apt update
+  sudo apt install -y unity-tweak-tool
 
   # Set window shortcuts
   # ------------- Window-management shortcuts -------------
