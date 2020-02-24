07de96d4c42ecf977df391a06a13f67e0546eec1
nikonikolov <niko.b.nikolov@gmail.com>
2020-02-24 22:17:20 +0000

    intel GPU for ubuntu

---
+++ ubuntu/use_intel_for_X.sh
@@ -0,0 +1,13 @@
+sudo cat > /etc/X11/xorg.conf <<- EOM
+Section "Device"
+    Identifier      "intel"
+    Driver          "intel"
+    BusId           "PCI:0:2:0"
+EndSection
+
+Section "Screen"
+    Identifier      "intel"
+    Device          "intel"
+EndSection
+
+EOM
